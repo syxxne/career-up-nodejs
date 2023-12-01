4c10ac195d96a805bedaf5f74651384ddd31d3cd
syxxne <tjdus562@naver.com>
2023-12-01 20:00:20 +0900

    modify oepnvidu_url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = express();
 // Environment variable: PORT where the node server is listening
 var SERVER_PORT = 5001;
 // Environment variable: URL where our OpenVidu server is listening
-var OPENVIDU_URL = "https://openvidu.career-up.live";
+var OPENVIDU_URL = "https://openvidu.career-up.live:8443";
 // Environment variable: secret shared with our OpenVidu server
 var OPENVIDU_SECRET = "CAREER-UP";
 
